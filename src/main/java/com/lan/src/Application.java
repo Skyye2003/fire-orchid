b23bedb5ba3b84f44209e3d4e679fb857e6a4a88
叶翔 <1245425142@qq.com>
2023-08-27 23:02:46 +0800

    init

---
+++ src/main/java/com/lan/src/Application.java
@@ -0,0 +1,13 @@
+package com.lan.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lan/src/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lan.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
