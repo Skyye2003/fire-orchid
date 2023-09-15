1142e1b99535bb6043eef9655ad885b217659280
jittsco <173189397@qq.com>
2023-09-15 23:16:12 +0800

    add: 部分代码注释

--- src/main/java/com/lan/src/service/impl/DirectoryServiceImpl.java
+++ src/main/java/com/lan/src/service/impl/DirectoryServiceImpl.java
@@ -20,16 +20,22 @@ public class DirectoryServiceImpl implements IDirectoryService {
     @Override
     public Result<List<Object>> getRoot() {
         List<String> temp = new ArrayList<>();
+        //查询
         DiskContent diskContent = diskContentMapper.selectByPrimaryKey(3);
+        //获取登记项字符串
         String content = diskContent.getContent();
         int conLen = content.length(),start = 0,end = 8;
+        //切割
         while(start<conLen&&end<conLen){
             temp.add(content.substring(start,end));
+            //指向下一个待切割登记项
             start+=9;
             end+=9;
         }
+        //处理最后一个带切割登记项
         temp.add(content.substring(start));
         List<Object> result = new ArrayList<>();
+        //生成文件/目录实体类
         for (String str : temp) {
 
         }
--- src/main/java/com/lan/src/utils/ParseUtils.java
+++ src/main/java/com/lan/src/utils/ParseUtils.java
@@ -15,11 +15,15 @@ public class ParseUtils {
      * @param str 登记项
      */
     public static void parseAttribute(String str, boolean isDirectory){
+        //切割登记项
         String name = str.substring(0,3);
         String type = str.substring(3,5);
         String attribute = String.valueOf(str.charAt(5));
         String startId = String.valueOf(str.charAt(6));
         String len = String.valueOf(str.charAt(7));
+        //创建实体类、属性赋值
+        //...
+
         return;
     }
 }
