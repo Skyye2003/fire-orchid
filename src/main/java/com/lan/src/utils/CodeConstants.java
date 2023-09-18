3f8059653ab8a33ba38fa292b665c0dd700022f2
jittsco <173189397@qq.com>
2023-09-18 17:53:48 +0800

    add:删除子目录接口

--- src/main/java/com/lan/src/utils/CodeConstants.java
+++ src/main/java/com/lan/src/utils/CodeConstants.java
@@ -4,16 +4,25 @@ public class CodeConstants {
     /**
      * 创建成功
      */
-    public static final Integer CREATE_OK = 20010;
+    public static final String CREATE_OK = "20010";
     /**
      * 创建文件名字超出长度限制(length = 3)
      */
-    public static final Integer CREATE_ERROR_NAME_OUT_OF_LEN = 20011;
+    public static final String CREATE_ERROR_NAME_OUT_OF_LEN = "20011";
     /**
      * 无空盘块分配
      */
-    public static final Integer CREATE_ERROR_NO_EMPTY = 20012;
+    public static final String CREATE_ERROR_NO_EMPTY = "20012";
     /**
-     *
+     * 删除成功
      */
+    public static final String DEL_OK = "30010";
+    /**
+     * 删除超出长度限制(length = 3)
+     */
+    public static final String DEL_ERROR_OUT_OF_LEN = "30011";
+    /**
+     * 删除目录不为空
+     */
+    public static final String DEL_ERROR_NOT_EMPTY = "30012";
 }
