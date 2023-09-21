dfee4fbed63bdb88a185e7c45715e3b7213a006b
jittsco <173189397@qq.com>
2023-09-21 10:54:43 +0800

    fix: 打开文件接口

--- src/main/java/com/lan/src/service/IFileService.java
+++ src/main/java/com/lan/src/service/IFileService.java
@@ -1,18 +1,17 @@
 package com.lan.src.service;
 
-import com.lan.src.dto.CreFileDTO;
-import com.lan.src.dto.DelFileDTO;
-import com.lan.src.dto.OpenFileDTO;
-import com.lan.src.dto.RegistryDto;
+import com.lan.src.dto.*;
 import com.lan.src.pojo.FileInfo;
 import com.lan.src.pojo.Result;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public interface IFileService {
     Result<RegistryDto> createFile(CreFileDTO creFileDTO);
 
-    Result<FileInfo> openFile(OpenFileDTO openFileDTO);
+    Result<FileInfoDTO> openFile(OpenFileDTO openFileDTO);
 
     Result<String> delFile(DelFileDTO delFileDTO);
 }
